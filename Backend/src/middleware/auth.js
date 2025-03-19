a0446ee6e1aa39436228b46cbccf58b5af43e39e
Fadi-Jawhar <fadi_jawhar@hotmail.com>
2025-03-19 12:29:17 +0100

    add front and backend

---
+++ Backend/src/middleware/auth.js
@@ -0,0 +1,11 @@
+import jwt from 'jsonwebtoken';
+
+export const auth = async (req, res, next) => {
+  //TODO Implement authentication
+  return next();
+};
+
+export const adminAuth = async (req, res, next) => {
+  //TODO Implement admin authentication
+  return next();
+};
