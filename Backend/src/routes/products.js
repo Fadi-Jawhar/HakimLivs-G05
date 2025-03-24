0ec4578df4c988673cb8e27fe9143161c7a9cb43
Felix0291 <felix.tapper@gmail.com>
2025-03-24 10:19:55 +0100

    .post to product controller

--- Backend/src/routes/products.js
+++ Backend/src/routes/products.js
@@ -7,20 +7,7 @@ const productsRouter = express.Router();
 productsRouter.get("/", getAllProducts);
 
 // Create product (admin only)
-productsRouter.post("/", adminAuth, async (req, res) => {
-  try {
-    const product = new Product(req.body);
-    await product.save();
-    res.status(201).json(product);
-  } catch (error) {
-    res.status(400).json({ error: error.message });
-  }
-});
 productsRouter.post("/", createProducts)
-
-// endpoint: api/products/id
-productsRouter.delete('/:id', deleteProduct)
-// productsRouter.put('/:id', updateProduct)
 
 //TODO Update product (admin only)
 
