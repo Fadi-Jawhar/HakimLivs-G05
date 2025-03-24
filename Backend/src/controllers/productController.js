d07807658dd004715cacc0e0cb3fc0f53947c286
Enya-Sover <David.kagell@yh.nackademin.se>
2025-03-24 12:07:50 +0100

    små fix

--- Backend/src/controllers/productController.js
+++ Backend/src/controllers/productController.js
@@ -25,19 +25,19 @@ export const createProducts = async (req, res) => {
 
 export const deleteProduct = async(req, res)=>{
   const {id} = req.params
+  // Valiterar att id:t är i rätt format innan try/catch
   if(!mongoose.Types.ObjectId.isValid(id)){
-    return res.status(404).json({sucess: false, message: 'Product not found'})
+    return res.status(404).json({success: false, message: 'Product not found'})
 } 
   try {
-      const deletedProduct = await Product.findOneAndDelete(id)
+      const deletedProduct = await Product.findByIdAndDelete(id)
       if(!deletedProduct){
-        console.log('Product not found')
-        return res.status(500).json({error: 'Product not found'})
+        return res.status(404).json({error: 'Product not found'})
       }
-      res.status(200).json({message: 'Product deleted successfully', deletedProduct})
+      res.status(200).json({success: true, message: 'Product deleted successfully'})
     } catch (error) {
       console.error(error)
-      res.status(500).json({ error: "Internal server error" })
+      res.status(500).json({ success: false, error: "Internal server error" })
     }
 }
 
