b2716a821df9372a3ee1e2d65aafecfe27677e49
Fadi-Jawhar <fadi.jawhr@hotmail.com>
2025-04-03 17:42:04 +0200

    added admin dashboard and all functions

--- Frontend/src/utils/api.js
+++ Frontend/src/utils/api.js
@@ -1,17 +1,19 @@
 export function getBaseUrl() {
   if (!window.location.href.includes('localhost')) {
-    return "https://hakim-livs-g05-be.vercel.app/"
+    return "https://hakim-livs-g05-be.vercel.app/";
   }
   return "http://localhost:3000/";
 }
 
-export async function fetchProducts(endpoint = "api/products") {
-  //! DONT USE THIS IN PRODUCTION
-  const url = `${getBaseUrl()}${endpoint}`;
-  const response = await fetch(url);
-  if(response.ok){
-    const data = await response.json();
-    return data;
+
+export async function fetchProducts() {
+  try {
+    const res = await fetch(`${getBaseUrl()}api/products`);
+    if (!res.ok) throw new Error("Kunde inte hämta produkter");
+    return await res.json();
+  } catch (err) {
+    console.error(err);
+    return [];
   }
-  return [];    
 }
+
