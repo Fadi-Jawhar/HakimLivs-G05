2174d35064c6544e94878ff8224f95c2e9a96ef0
Enya-Sover <David.kagell@yh.nackademin.se>
2025-04-17 14:17:48 +0200

    Fixar rendering av orderhistorik

---
+++ Frontend/src/scripts/orderHistory.js
@@ -0,0 +1,42 @@
+
+const tbody = document.querySelector(".table-body");
+
+const fetchUser = async (id) => {
+    const token = localStorage.getItem("token");
+    const response = await fetch(`https://hakim-livs-g05-be.vercel.app/api/user/${id}`, {
+        headers: {
+            "Authorization": `Bearer ${token}`
+        }
+    });
+    const data = await response.json();
+    console.log(data)
+   return data
+}
+const fetchOrderHistory = async () => {
+
+    const token = localStorage.getItem("token");
+    const response = await fetch("https://hakim-livs-g05-be.vercel.app/api/order", {
+        headers: {
+            "Authorization": `Bearer ${token}`
+        }
+    });
+    const data = response.ok ? await response.json() : [];
+    console.log(data)
+    const rows = await Promise.all(
+        data.map(async (order) => {
+          const user = await fetchUser(order.user);
+          return `
+            <tr>
+              <td>${order._id}</td>
+              <td>${user.username}</td>
+              <td>${order.totalAmount}</td>
+              <td>${new Date(order.createdAt).toLocaleString()}</td>
+              <td>${user.lojaltyBonus ? 'Trogen kund' : 'Ej trogen kund'}</td>
+            </tr>`;
+        })
+      );
+      tbody.innerHTML = rows.join("");
+
+}
+fetchOrderHistory()
+
